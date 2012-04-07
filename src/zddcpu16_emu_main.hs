b5267cdbc0511ed53227aa0b704fb4a94bf8ef3e
Luis Cabellos <zhen.sydow@gmail.com>
2012-04-07 14:15:51 +0200

    simple emulator with step debug

--- src/zddcpu16_emu_main.hs
+++ src/zddcpu16_emu_main.hs
@@ -15,10 +15,48 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----------------------------------------------------------------------------- -}
-module Main( main ) where
+module Main where
+
+-- -----------------------------------------------------------------------------
+import Data.Word( Word16 )
+import qualified Graphics.UI.SDL as SDL(
+  Event(..), SDLKey(..), Keysym(..), waitEvent )
+import ZDCpu16.EmuRender(
+  RenderState, runRender, mkRenderState, clearScreen, renderEmuState )
+import ZDCpu16.EmuState( EmuState(..), mkEmuState )
+import ZDCpu16.Hardware( loads )
+import ZDCpu16.ZDCpu16( runEmulator, stepEmulator )
+
+-- -----------------------------------------------------------------------------
+testProgram :: [Word16]
+testProgram = [0x7c01, 0x0030, 0x7de1, 0x1000, 0x0020, 0x7803, 0x1000, 0xc00d, 0x7dc1, 0x001a, 0xa861, 0x7c01, 0x2000, 0x2161, 0x2000, 0x8463, 0x806d, 0x7dc1, 0x000d, 0x9031, 0x7c10, 0x0018, 0x7dc1, 0x001a, 0x9037, 0x61c1, 0x7dc1, 0x001a, 0x0000, 0x0000, 0x0000, 0x0000]
+
+-- -----------------------------------------------------------------------------
+mainLoop :: RenderState -> EmuState -> IO ()
+mainLoop rst est = do
+  _ <- runRender (clearScreen >> renderEmuState est) rst
+  e <- SDL.waitEvent
+  case e of
+    SDL.Quit -> return ()
+    SDL.KeyUp key -> do
+      case SDL.symKey key of
+        SDL.SDLK_ESCAPE -> return ()
+        SDL.SDLK_q -> return ()
+        SDL.SDLK_s -> do
+          let (_,newEst) = runEmulator stepEmulator est
+          mainLoop rst newEst
+        _ -> do
+          print key
+          mainLoop rst est
+    _ -> mainLoop rst est
 
 -- -----------------------------------------------------------------------------
 main :: IO ()
-main = print "ok"
+main = do
+  rst <- mkRenderState
+  let initialEmuState = mkEmuState {
+        emuCpu = loads 0 testProgram $ emuCpu mkEmuState }
+  mainLoop rst initialEmuState
+  putStrLn "Exit"
 
 -- -----------------------------------------------------------------------------
