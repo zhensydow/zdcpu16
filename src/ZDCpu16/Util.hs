727f870a533c7e909ef72835822edad0eef8f6ed
Luis Cabellos <cabellos@ifca.unican.es>
2012-04-09 17:33:35 +0200

    add [Word16] to bytestring conversion

--- src/ZDCpu16/Util.hs
+++ src/ZDCpu16/Util.hs
@@ -15,11 +15,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----------------------------------------------------------------------------- -}
-module ZDCpu16.Util( showWord, byteStringToWord16 ) where
+module ZDCpu16.Util(
+  showWord, byteStringToWord16, word16ToByteString ) where
 
 -- -----------------------------------------------------------------------------
-import qualified Data.ByteString as BS( ByteString, unpack )
-import Data.Bits( shiftL, (.|.) )
+import qualified Data.ByteString as BS( ByteString, unpack, pack )
+import Data.Bits( shiftR, shiftL, (.|.), (.&.) )
 import Data.Word( Word8, Word16 )
 import Numeric( showHex )
 
@@ -34,6 +35,9 @@ showWord v = replicate filln '0' ++ cad
 byteStringToWord16 :: BS.ByteString -> [Word16]
 byteStringToWord16 = packW8ToW16 . BS.unpack
 
+word16ToByteString :: [Word16] -> BS.ByteString
+word16ToByteString = BS.pack . packW16ToW8
+
 -- -----------------------------------------------------------------------------
 packW8ToW16 :: [Word8] -> [Word16]
 packW8ToW16 [] = []
@@ -42,5 +46,13 @@ packW8ToW16 (x:y:xs) = word : packW8ToW16 xs
   where
     ah = fromIntegral x `shiftL` 8
     al = fromIntegral y
-    word =  ah .|. al 
+    word =  ah .|. al
+-- -----------------------------------------------------------------------------
+packW16ToW8 :: [Word16] -> [Word8]
+packW16ToW8 [] = []
+packW16ToW8 (x:xs) = ah : al : packW16ToW8 xs
+  where
+    ah = fromIntegral $ (x `shiftR` 8) .&. 0xff
+    al = fromIntegral $ x .&. 0xff
+
 -- -----------------------------------------------------------------------------
